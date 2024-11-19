7f7c2510e3b275a13c001f321df2db6f82f490ec
C0B23095 <c0b23095c0@edu.teu.ac.jp>
2024-11-19 15:29:56 +0900

    練習１

---
+++ pygame_image.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    img = pg.image.load("fig/3.png")
+    
+    enn = pg.Surface((20,20))
+    pg.transform.flip(img, -1, 1)
+    pg.draw.circle(enn, (255, 0, 0), (10,10),10)
+    
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(img, [300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
